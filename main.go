055606a32413d6e2cf0944c94c7f7d48e27c84c5
ArtemisNyx3 <artemisnyx3@yahoo.com>
2025-04-15 17:41:51 -0400

    reorg

--- main.go
+++ main.go
@@ -59,3 +59,24 @@ func cleanInput(text string) []string {
 	}
 	return cleanIP
 }
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Printf("Usage:\n\n")
+	for name, cmd := range cliDirectory {
+		fmt.Printf("%s: %s\n", name, cmd.description)
+	}
+	return nil
+}
